7617e11167a029b70c5fb37d639d3d42682132dc
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2020-03-24 17:46:02 +0100

    Use io.Pipe to export and upload concurrently

--- server/command_hooks.go
+++ server/command_hooks.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"io"
+	"strconv"
 	"strings"
 	"time"
 
@@ -65,9 +67,12 @@ func (p *Plugin) executeCommandExport(args *model.CommandArgs) *model.CommandRes
 		}
 	}
 
+	pipeReader, pipeWriter := io.Pipe()
+	fileName := fmt.Sprintf("%d_%s.csv", time.Now().Unix(), channelToExport.Name)
+
 	go func() {
-		fileName := fmt.Sprintf("%d_%s.json", time.Now().Unix(), channelToExport.Name)
-		fileContents, err := p.exportChannel(channelToExport)
+		defer pipeWriter.Close()
+		err := p.exportChannel(channelToExport, pipeWriter)
 		if err != nil {
 			p.client.Post.CreatePost(&model.Post{
 				UserId:    p.botID,
@@ -77,8 +82,10 @@ func (p *Plugin) executeCommandExport(args *model.CommandArgs) *model.CommandRes
 
 			return
 		}
+	}()
 
-		file, err := p.uploadExportedChannelTo(fileName, fileContents, args.UserId)
+	go func() {
+		file, err := p.uploadExportedChannelTo(fileName, pipeReader, args.UserId)
 		if err != nil {
 			p.client.Post.CreatePost(&model.Post{
 				UserId:    p.botID,
@@ -104,14 +111,117 @@ func (p *Plugin) executeCommandExport(args *model.CommandArgs) *model.CommandRes
 	}
 }
 
-// Export an empty JSON file for now. The actual implementation will come later.
-func (p *Plugin) exportChannel(channel *model.Channel) (io.Reader, error) {
-	fileContents := strings.NewReader("{}")
-	return fileContents, nil
+func (p *Plugin) exportChannel(channel *model.Channel, writer io.Writer) error {
+	csvWriter := csv.NewWriter(writer)
+	err := csvWriter.Write([]string{
+		"Post Creation Time",
+		"User Id",
+		"User Email",
+		"User Type",
+		"User Name",
+		"Post Id",
+		"Parent Post Id",
+		"Post Message",
+		"Post Type",
+	})
+
+	if err != nil {
+		return fmt.Errorf("Unable to create a CSV file: %w", err)
+	}
+
+	for page, perPage := 0, 100; ; page++ {
+		// FIXME: Swap page and perPage parameters when https://github.com/mattermost/mattermost-server/pull/14125 is released
+		postList, err := p.client.Post.GetPostsForChannel(channel.Id, page, perPage)
+
+		if err != nil {
+			return fmt.Errorf("Unable to fetch page %d (with %d posts) from channel %s: %w", 0, 2, channel.Id, err)
+		}
+
+		for _, key := range postList.Order {
+			post := postList.Posts[key]
+			// Ignore posts that have been edited; exporting only what's visible in the channel
+			if post.OriginalId != "" {
+				continue
+			}
+
+			exportedPost, err := p.exportedPost(post)
+			if err != nil {
+				return fmt.Errorf("Unable to export post: %w", err)
+			}
+
+			line := postToCSVLine(exportedPost)
+			csvWriter.Write(line)
+		}
+
+		if len(postList.Posts) < perPage {
+			break
+		}
+	}
+
+	csvWriter.Flush()
+
+	return nil
 }
 
-func (p *Plugin) uploadExportedChannelTo(fileName string, fileContents io.Reader, receiverID string) (*model.FileInfo, error) {
-	file, err := p.client.File.Upload(fileContents, fileName, receiverID)
+type ExportedPost struct {
+	CreateAt     int64
+	UserId       string
+	UserEmail    string
+	UserType     string
+	UserName     string
+	Id           string
+	ParentPostId string
+	Message      string
+	Type         string
+}
+
+func (p *Plugin) exportedPost(post *model.Post) (*ExportedPost, error) {
+	user, err := p.client.User.Get(post.UserId)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed retrieving post's author information")
+	}
+
+	userType := "user"
+	if user.IsBot {
+		userType = "bot"
+	}
+
+	postType := "message"
+	if post.Type != "" {
+		postType = post.Type
+		userType = "system"
+	}
+
+	return &ExportedPost{
+		CreateAt:     post.CreateAt,
+		UserId:       post.UserId,
+		UserEmail:    user.Email,
+		UserType:     userType,
+		UserName:     user.Nickname,
+		Id:           post.Id,
+		ParentPostId: post.ParentId,
+		Message:      post.Message,
+		Type:         postType,
+	}, nil
+}
+
+func postToCSVLine(post *ExportedPost) []string {
+	line := []string{
+		strconv.FormatInt(post.CreateAt, 10),
+		post.UserId,
+		post.UserEmail,
+		post.UserType,
+		post.UserName,
+		post.Id,
+		post.ParentPostId,
+		post.Message,
+		post.Type,
+	}
+	return line
+}
+
+func (p *Plugin) uploadExportedChannelTo(fileName string, contents io.Reader, receiverID string) (*model.FileInfo, error) {
+	file, err := p.client.File.Upload(contents, fileName, receiverID)
 	if err != nil {
 		p.client.Log.Error("unable to upload the exported file to the channel",
 			"Channel ID", receiverID, "Error", err)
