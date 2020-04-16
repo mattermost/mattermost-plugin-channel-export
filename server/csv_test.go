da13b7ba01618047f0c0e0cc1cdde4cf1d17e6b4
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2020-04-16 12:06:36 +0200

    MM-23110 CSV Exporter (#3)
    * Use io.Pipe to export and upload concurrently
    
    * Isolate exporter interface and CSV implementation
    
    * Make exporters independent of the plugin client
    
    - Build an iterator from the posts of the channel and pass it to the exporter
    
    * Reorganize, clean and document code
    
    - Add FileName function
    - Change batch size owner
    - Move functions to correct file
    - Fix go.mod
    - Document public and important functions
    
    * Test CSVExporter
    
    * Increase number of posts retrieved per API call
    
    * Use errors New and Wrap instead of fmt.Errorf
    
    * Convert creation timestamps to time.Time
    
    * Rename CSVExporter to CSV
    
    * Move exportedPost to TestExport
    
    * Add a users cache
    
    * go mod tidy
    
    * Propagate the writer error to the reader
    
    Use CloseWithError in the writer end of the pipe to inform the reader
    end that there was an error in the process.
    Use a specific type of error, sent by the writer and checked by the
    reader, to make sure that only one error post is sent to the user.
    
    * Add "CSV" to tests name
    
    * Modify users cache to a local, per export, map
    
    This reverts commit e5ff6f0698ec3b87d668e0033580ceb242c5fdbc.

---
+++ server/csv_test.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"errors"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCSVFileName(t *testing.T) {
+	exporter := CSV{}
+
+	testCases := []struct {
+		testName         string
+		name             string
+		expectedFilename string
+	}{
+		{"Empty name", "", ".csv"},
+		{"Normal name", "name", "name.csv"},
+		{"Name with unicode chars", "αβ", "αβ.csv"},
+		{"Name with digits", "1", "1.csv"},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.testName, func(*testing.T) {
+			require.Equal(t, exporter.FileName(test.name), test.expectedFilename)
+		})
+	}
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+func exportedPostToCSV(post *ExportedPost) string {
+	fields := []string{
+		post.CreateAt.String(),
+		post.UserID,
+		post.UserEmail,
+		post.UserType,
+		post.UserName,
+		post.ID,
+		post.ParentPostID,
+		post.Message,
+		post.Type,
+	}
+	return strings.Join(fields, ",") + "\n"
+}
+
+func TestCSVExport(t *testing.T) {
+	header := []string{
+		"Post Creation Time",
+		"User Id",
+		"User Email",
+		"User Type",
+		"User Name",
+		"Post Id",
+		"Parent Post Id",
+		"Post Message",
+		"Post Type",
+	}
+	headerCSV := strings.Join(header, ",") + "\n"
+
+	var exportedPost = &ExportedPost{
+		CreateAt:     time.Now(),
+		UserID:       "dummyUserID",
+		UserEmail:    "dummy@email.com",
+		UserType:     "user",
+		UserName:     "dummy",
+		ID:           "dummyPostID",
+		ParentPostID: "",
+		Message:      "Lorem ipsum",
+		Type:         "message",
+	}
+
+	genIterator := func(numPosts, batchSize int) PostIterator {
+		sent := 0
+		return func() ([]*ExportedPost, error) {
+			if sent >= numPosts {
+				return nil, nil
+			}
+
+			length := min(numPosts-sent, batchSize)
+
+			posts := make([]*ExportedPost, length)
+			for i := 0; i < length; i++ {
+				posts[i] = exportedPost
+			}
+
+			sent += length
+			return posts, nil
+		}
+	}
+
+	exporter := CSV{}
+
+	t.Run("Empty iterator", func(t *testing.T) {
+		var actualString strings.Builder
+
+		err := exporter.Export(genIterator(0, 0), &actualString)
+
+		require.Nil(t, err)
+		require.Equal(t, headerCSV, actualString.String())
+	})
+
+	t.Run("One post", func(t *testing.T) {
+		var actualString strings.Builder
+
+		err := exporter.Export(genIterator(1, 1), &actualString)
+
+		require.Nil(t, err)
+		require.Equal(t, headerCSV+exportedPostToCSV(exportedPost), actualString.String())
+	})
+
+	t.Run("Several posts", func(t *testing.T) {
+		var actualString strings.Builder
+
+		err := exporter.Export(genIterator(10, 4), &actualString)
+
+		expected := headerCSV
+		for i := 0; i < 10; i++ {
+			expected += exportedPostToCSV(exportedPost)
+		}
+
+		require.Nil(t, err)
+		require.Equal(t, expected, actualString.String())
+	})
+
+	t.Run("Wrong iterator", func(t *testing.T) {
+		var actualString strings.Builder
+
+		err := exporter.Export(
+			func() ([]*ExportedPost, error) { return nil, errors.New("forcing an error") },
+			&actualString,
+		)
+
+		require.Error(t, err)
+	})
+}
---
+++ server/exporter.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"io"
+	"time"
+
+	"github.com/mattermost/mattermost-server/v5/model"
+	"github.com/pkg/errors"
+)
+
+// PostIterator returns the next batch of posts when called
+type PostIterator func() ([]*ExportedPost, error)
+
+// Exporter processes a list of posts and writes them to a writer
+type Exporter interface {
+	// FileName returns the name of the exported file, given the core name passed
+	FileName(name string) string
+
+	// Export processes the posts returned by nextPosts and exports them to writer
+	Export(nextPosts PostIterator, writer io.Writer) error
+}
+
+// ExportedPost contains all the information from a post needed in
+// an export, with all the relevant information already resolved
+type ExportedPost struct {
+	CreateAt     time.Time
+	UserID       string
+	UserEmail    string
+	UserType     string
+	UserName     string
+	ID           string
+	ParentPostID string
+	Message      string
+	Type         string
+}
+
+// channelPostsIterator returns a function that returns, every time it is
+// called, a new batch of posts from the channel, chronollogically ordered
+// (most recent first), until all posts have been consumed.
+func (p *Plugin) channelPostsIterator(channel *model.Channel) PostIterator {
+	usersCache := make(map[string]*model.User)
+	page := 0
+	perPage := 1000
+	return func() ([]*ExportedPost, error) {
+		postList, err := p.client.Post.GetPostsForChannel(channel.Id, page, perPage)
+		if err != nil {
+			return nil, err
+		}
+
+		exportedPostList := make([]*ExportedPost, 0, len(postList.Order))
+		for _, key := range postList.Order {
+			post := postList.Posts[key]
+
+			// Ignore posts that have been edited; exporting only what's visible in the channel
+			if post.OriginalId != "" {
+				continue
+			}
+
+			exportedPost, err := p.toExportedPost(post, usersCache)
+			if err != nil {
+				return nil, errors.Wrap(err, "unable to export post")
+			}
+
+			exportedPostList = append(exportedPostList, exportedPost)
+		}
+
+		page++
+		return exportedPostList, nil
+	}
+}
+
+// toExportedPost resolves all the data from post that is needed in
+// ExportedPost, as the user information and the type of message
+func (p *Plugin) toExportedPost(post *model.Post, usersCache map[string]*model.User) (*ExportedPost, error) {
+	user, ok := usersCache[post.UserId]
+	if !ok {
+		newUser, err := p.client.User.Get(post.UserId)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed retrieving post's author information")
+		}
+
+		usersCache[post.UserId] = newUser
+		user = newUser
+	}
+
+	userType := "user"
+	if user.IsBot {
+		userType = "bot"
+	}
+
+	postType := "message"
+	if post.Type != "" {
+		postType = post.Type
+		userType = "system"
+	}
+
+	seconds := post.CreateAt / 1e3
+	nanoseconds := (post.CreateAt % 1e3) * 1e9
+	createAt := time.Unix(seconds, nanoseconds)
+
+	return &ExportedPost{
+		CreateAt:     createAt,
+		UserID:       post.UserId,
+		UserEmail:    user.Email,
+		UserType:     userType,
+		UserName:     user.Nickname,
+		ID:           post.Id,
+		ParentPostID: post.ParentId,
+		Message:      post.Message,
+		Type:         postType,
+	}, nil
+}
