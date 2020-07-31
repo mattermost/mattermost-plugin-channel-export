33dda9f0c335979e284ddac92f13be5d5481b4e6
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2020-07-31 17:40:41 +0200

    Fix dependencies, imports and types

--- tests-e2e/cypress/plugins/index.ts
+++ tests-e2e/cypress/plugins/index.ts
@@ -1,13 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-/// <reference types="cypress" />
-
 // eslint-disable-next-line no-unused-vars
-/**
- * @type {Cypress.PluginConfig}
- */
-module.exports = (on, config) => {
+module.exports = (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) => {
     // `on` is used to hook into various events Cypress emits
     // `config` is the resolved Cypress config
 };
